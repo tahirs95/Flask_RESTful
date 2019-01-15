5de03970efbc71a94db3c15c178611177fe40717
tahirs95 <tahirs95@hotmail.com>
2019-01-15 19:31:51 +0500

    Flask-RESTful work.

--- app.py
+++ app.py
@@ -1,14 +1,67 @@
 from flask import Flask, jsonify, request, render_template
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
+from flask_jwt import JWT, jwt_required
+from security import authenticate, identity
 
 app = Flask(__name__)
-
+app.secret_key = "jose"
 api = Api(app)
 
-class Student(Resource):
+jwt = JWT(app, authenticate, identity)
+
+items = []
+
+class Item(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('price',
+        type=float,
+        required = True,
+        help= "This field cannot be left blank."
+    )
+    @jwt_required()
     def get(self, name):
-        return ({"student":name})
+        # for item in items:
+        #     if item['name'] == name:
+        #         return item
+        item = next(filter(lambda x : x["name"] == name, items), None) #next is used to fetch from first item else use "list"
+        return {"item":item}, 200 if item is not None else 404
+    
+    def post(self,name):
+        
+        
+        if next(filter(lambda x : x["name"] == name, items), None):
+            return({"message":"An item with name {} already exists.".format(name)})
+        data = Item.parser.parse_args()
+        # data = request.get_json()
+        #force=True, silent=True
+        item = {"name":data["name"], "price":data["price"]}
+        items.append(item)
+        return item, 201
+    
+    def delete(self, name):
+        global items
+        items = list(filter(lambda x: x["name"] != name, items))
+        return {"message":"Item deleted"}
 
-api.add_resource(Student, '/student/<string:name>')
+    def put(self, name):
+        data = Item.parser.parse_args()
+        # data = request.get_json()
+        item = next(filter(lambda x: x["name"] == name, items), None)
+        if item is None:
+            item = {"name":data['name'], "price":data['price']}
+            items.append(item)
+        else:
+            item.update(data)
+        return item
 
-app.run(port=5000)
+
+    
+class ItemList(Resource):
+    def get(self):
+        return {"items":items}
+
+
+api.add_resource(Item, '/item/<string:name>','/item')
+api.add_resource(ItemList, '/items')
+
+app.run(port=5000, debug=True)
